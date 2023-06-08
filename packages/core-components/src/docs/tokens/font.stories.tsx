849fcff63718b5a1505e070d090f26e3aac6021f
Maria Duenas <mduenias@thoughtworks.com>
2023-06-08 17:32:35 +0200

    chore(docs): display pixel value for font sizes in docs

--- packages/core-components/src/docs/tokens/font.stories.tsx
+++ packages/core-components/src/docs/tokens/font.stories.tsx
@@ -4,47 +4,73 @@ import allTokens from '@otto-de/b2b-tokens/build/js/default.json';
 import './styles.css';
 import fontDocs from './font.docs.mdx';
 
-const tokenRow = token => {
+const tokenRow = (token, size?) => {
   return html`
     <tr>
       <td>${token.name}</td>
       <td>${token.value}</td>
+      ${size && html`<td>${size}</td>`}
     </tr>
   `;
 };
 
-const tokenTable = (tokensByType, tokenGroup, groupName?) => {
-  return html`
-    ${groupName && html`<b2b-headline size="200">${groupName}</b2b-headline>`}
-    <br />
-    <table style="width: 600px;">
-      ${html`
-        ${tokenGroup.map(type => {
-          const tokens = tokensByType[type];
-          return html`
-            <tr>
-              <td colspan="3" class="token-type">${type}</td>
-            </tr>
-            ${tokens.map(token => tokenRow(token))}
-          `;
-        })}
-      `}
-    </table>
-  `;
-};
+const semanticTokens = html`
+  <b2b-headline size="200">Semantic tokens</b2b-headline>
+  <br />
+  <table style="width: 600px;">
+    ${html`
+      ${['family', 'weight'].map(type => {
+        const tokens = allTokens.font[type];
+        return html`
+          <tr>
+            <td colspan="3" class="token-type">${type}</td>
+          </tr>
+          ${tokens.map(token => tokenRow(token))}
+        `;
+      })}
+    `}
+  </table>
+`;
+
+const sizeTokens = html`
+  <table style="width: 600px;">
+    <thead>
+      <tr>
+        <th>Token Name</th>
+        <th>Value in rem</th>
+        <th>Value in px</th>
+      </tr>
+    </thead>
+    ${html`
+      ${['copy', 'headline'].map(type => {
+        const tokens = allTokens.size[type];
+        return html`
+          <tr>
+            <td colspan="3" class="token-type">${type}</td>
+          </tr>
+          ${tokens.map(token => {
+            console.log(token.value);
+            const valueInPixels = parseFloat(token.value) * 16 + 'px';
+            return tokenRow(token, valueInPixels);
+          })}
+        `;
+      })}
+    `}
+  </table>
+`;
 
 const FontTokens: Story = ({}) => {
   return html`
     <b2b-headline>Font</b2b-headline>
     <br />
-    ${tokenTable(allTokens.font, ['family', 'weight'], 'Semantic tokens')}
+    ${semanticTokens}
     <br /><br />
     <b2b-headline size="100">Font sizes and line-height</b2b-headline>
     <b2b-paragraph
       >Please match the font size with the line-height of the same
       level.</b2b-paragraph
     >
-    ${tokenTable(allTokens.size, ['copy', 'headline'])}
+    ${sizeTokens}
   `;
 };
 
