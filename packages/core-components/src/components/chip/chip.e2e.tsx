8dec811a47e4b799524d9ca22fc26022d6a67bda
Anna Hoege <anna.hoege@otto.de>
2023-11-16 10:18:32 +0100

    test(Chip): [B2BDS-221] adjusted tests

--- packages/core-components/src/components/chip/chip.e2e.tsx
+++ packages/core-components/src/components/chip/chip.e2e.tsx
@@ -9,7 +9,7 @@ describe('B2B-Chip-Component', () => {
 
     const chip = await page.find({ text: 'chip' });
     const clearIcon = await page.find(
-      'b2b-chip-component >>> div.b2b-chip__clearIcon',
+      'b2b-chip-component >>> button.b2b-chip__clearIcon',
     );
     expect(chip).not.toBeNull();
     expect(clearIcon).not.toBeNull();
@@ -24,25 +24,11 @@ describe('B2B-Chip-Component', () => {
     const b2bClose = await page.spyOnEvent('b2b-close');
 
     const clearIcon = await page.find(
-      'b2b-chip-component >>>  div.b2b-chip__clearIcon',
+      'b2b-chip-component >>>  button.b2b-chip__clearIcon',
     );
 
     await clearIcon.click();
 
     expect(b2bClose).toHaveReceivedEvent();
-  });
-  it('should truncate the chip component text ', async () => {
-    const page = await newE2EPage();
-    await page.setContent(
-      `<b2b-chip-component label="chip with a long text" truncate='true' max-width='20px'></b2b-chip-component>`,
-    );
-
-    const chip = await page.find({ text: 'chip with a long text' });
-    const clearIcon = await page.find(
-      'b2b-chip-component >>> div.b2b-chip__clearIcon',
-    );
-    expect(chip).not.toBeNull();
-    expect(clearIcon).not.toBeNull();
-    expect(chip).toHaveClass('b2b-chip--truncate');
   });
 });
