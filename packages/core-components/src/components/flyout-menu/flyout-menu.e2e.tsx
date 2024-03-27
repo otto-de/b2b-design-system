6d383468ef4e2b9c373fe67014e9d6515dcc7dd8
Anna Hoege <anna.hoege@otto.de>
2024-03-27 14:30:02 +0100

    ci: downgraded puppeteer to last version before mandatory chrome usage

--- packages/core-components/src/components/flyout-menu/flyout-menu.e2e.tsx
+++ packages/core-components/src/components/flyout-menu/flyout-menu.e2e.tsx
@@ -59,7 +59,7 @@ describe('B2B-FlyoutMenu', () => {
   /** inexplicably, this test fails after upgrading Puppeteer to a supported version. Regarding the trade off
    * between using a supported version of Puppeteer and the value of this test, we will skip it for now.
    */
-  xit('should emit the selected event when a menu option is clicked', async () => {
+  it('should emit the selected event when a menu option is clicked', async () => {
     const optionSelectedEventSpy = await page.spyOnEvent('b2b-option-selected');
     const icon = await page.find('b2b-icon');
 
