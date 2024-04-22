2b4f7c22c54afc61c9336ec50125ddfe3bfb2fd0
vishwapriya <vishprij@thoughtworks.com>
2024-04-22 16:51:46 +0530

    feat(Calender): [BLA-693] Adds e2e test

---
+++ packages/core-components/src/components/calendar/calendar.e2e.tsx
@@ -0,0 +1,74 @@
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('B2B-Calendar', () => {
+  let page;
+  // let today = new Date();
+  beforeEach(async () => {
+    page = await newE2EPage();
+    await page.setContent(`<b2b-calendar></b2b-calendar>`);
+  });
+
+  it('should render the calendar component', async () => {
+    const calendar = await page.find('b2b-calendar');
+    const clickableInputComponent = await page.find(
+      'b2b-calendar >>> div.b2b-calender-input-wrapper',
+    );
+
+    expect(clickableInputComponent).not.toBeNull();
+    expect(calendar).not.toBeNull();
+  });
+
+  it('should render the datepicker component when calender icon is clicked', async () => {
+    const calendar = await page.find('b2b-calendar');
+    const clickableInputComponent = await page.find(
+      'b2b-calendar >>> div.b2b-calender-input-wrapper',
+    );
+
+    await clickableInputComponent.click();
+    await page.waitForChanges();
+
+    const datePickerComponent = await page.find(
+      'b2b-calendar >>> .b2b-datepicker',
+    );
+    const datePickerHeader = await page.find(
+      'b2b-calendar >>> .b2b-datepicker >>> b2b-calendar-header',
+    );
+    const datePickerDays = await page.find(
+      'b2b-calendar >>> .b2b-datepicker >>> b2b-calender-days',
+    );
+
+    expect(clickableInputComponent).not.toBeNull();
+    expect(calendar).not.toBeNull();
+
+    expect(datePickerComponent).not.toBeNull();
+    expect(datePickerHeader).not.toBeNull();
+    expect(datePickerDays).not.toBeNull();
+    expect(await datePickerComponent.isVisible()).toBe(true);
+    expect(await datePickerHeader.isVisible()).toBe(true);
+    expect(await datePickerDays.isVisible()).toBe(true);
+  });
+
+  // it('should render the datepicker component and display the previous month when left arrow is clicked', async () => {
+  //   const clickableInputComponent = await page.find(
+  //     'b2b-calendar >>> div.b2b-calender-input-wrapper',
+  //   );
+  //
+  //   await clickableInputComponent.click();
+  //   await page.waitForChanges();
+  //
+  //   let calendarHeaderButton = await page.find(
+  //     'b2b-calendar-header >>> button',
+  //   );
+  //   let monthNameRenderedBeforeClick = await page.find({ text: 'Apr' });
+  //
+  //   await calendarHeaderButton.click();
+  //
+  //   await page.waitForChanges();
+  //
+  //   let monthNameRendered = await page.find({ text: 'Mär' });
+  //
+  //   expect(calendarHeaderButton).not.toBeNull();
+  //   expect(monthNameRenderedBeforeClick).not.toBeNull();
+  //   expect(monthNameRendered).not.toBeNull();
+  // });
+});
