0193186fbda11293640669032406f34e3e30039c
vishwapriya <vishprij@thoughtworks.com>
2024-10-23 18:56:16 +0530

    refactor(DatePicker): [BLA-996] Remove the show hide functionality for close icon

--- packages/core-components/src/components/date-picker/date-picker.tsx
+++ packages/core-components/src/components/date-picker/date-picker.tsx
@@ -370,7 +370,6 @@ export class B2bDatePicker {
                     if (event.key === 'Enter') {
                       this.invalid = false;
                       this.clearDateInput();
-                      this.showHideDatePicker();
                     }
                   }}>
                   <b2b-icon
