8d3868c9cf8afb30657f8485cca10a74fde9bf10
vishwapriya <vishprij@thoughtworks.com>
2025-02-20 19:57:53 +0530

    fix(Grid): [BLA-1199] Calculate and set span for columns from parent

--- packages/core-components/src/components/grid/row.tsx
+++ packages/core-components/src/components/grid/row.tsx
@@ -26,6 +26,44 @@ export class B2bGridRowComponent {
     | 'space-between'
     | 'space-evenly' = 'start';
 
+  componentDidLoad() {
+    this.adjustColumnFlex();
+  }
+
+  private adjustColumnFlex() {
+    const columns = this.hostElement.querySelectorAll('b2b-grid-col');
+    let totalSpan = 0;
+    let columnsWithoutSpan: HTMLElement[] = [];
+
+    columns.forEach((column: any) => {
+      const span = column.getAttribute('span');
+      if (span) {
+        totalSpan += parseInt(span, 10);
+      } else {
+        columnsWithoutSpan.push(column);
+      }
+    });
+
+    const remainingSpan = 12 - totalSpan;
+
+    if (remainingSpan == 12) {
+      if (columnsWithoutSpan.length > 0) {
+        columnsWithoutSpan.forEach(column => {
+          column.setAttribute('span', '1');
+        });
+      }
+    }
+
+    if (columnsWithoutSpan.length > 0) {
+      const flexPerColumn =
+        ((remainingSpan / 12) * 100) / columnsWithoutSpan.length;
+
+      columnsWithoutSpan.forEach(column => {
+        column.style.flex = `${flexPerColumn}%`;
+      });
+    }
+  }
+
   render() {
     return (
       <Host
