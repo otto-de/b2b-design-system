63036599fa4bfae5f242761aa2e98bfd0b840931
Sofeiyakalaimathy <sofeiyakalaimathy.c@thoughtworks.com>
2025-06-04 10:32:30 +0530

    chore(stencil): [BLA-1409] Library and Dependencies (#578)

--- packages/tokens/build.mjs
+++ packages/tokens/build.mjs
@@ -1,8 +1,6 @@
-const fs = require('fs');
-const groupBy = require('lodash/groupBy');
-
-const StyleDictionaryPackage = require('style-dictionary');
-const { fileHeader, formattedVariables } = StyleDictionaryPackage.formatHelpers;
+import StyleDictionaryPackage from 'style-dictionary';
+import { fileHeader, formattedVariables } from 'style-dictionary/utils';
+import groupBy from 'lodash/groupBy.js';
 
 const themes = ['default', 'dark'];
 const platform = 'web';
@@ -13,22 +11,21 @@ const platform = 'web';
  */
 StyleDictionaryPackage.registerFormat({
   name: 'css/variables-themed',
-  formatter: function ({ dictionary, file, options }) {
+  format: function ({ dictionary, file, options }) {
     const { outputReferences, theme } = options;
     let selector = `:root [data-theme="${theme}"]`;
+
     if (theme === 'default' || theme === 'deprecated') {
       selector = `:root, :host`;
     }
-    return (
-      fileHeader({ file }) +
-      `${selector} {\n` +
-      formattedVariables({
+
+    return Promise.resolve(fileHeader({ file })).then(header => {
+      return `${header}\n${selector} {\n${formattedVariables({
         format: 'css',
         dictionary,
         outputReferences,
-      }) +
-      '\n}\n'
-    );
+      })}\n}\n`;
+    });
   },
 });
 
@@ -38,18 +35,18 @@ StyleDictionaryPackage.registerFormat({
  */
 StyleDictionaryPackage.registerFormat({
   name: 'javascript/docs',
-  formatter: function ({ dictionary, file, options }) {
+  format: function ({ dictionary, file, options }) {
     const groupedByCategory = groupBy(dictionary.allTokens, token => token.attributes.category);
     const groupedByColor = groupBy(groupedByCategory.color, token => token.attributes.type);
     const groupedByFont = groupBy(groupedByCategory.font, token => token.attributes.type);
     const groupedBySize = groupBy(groupedByCategory.size, token => {
-      // Testing if is a base value, current used for sizes as the base types could be grouped together
-      const isANumber = string => /\d/.test(string);
-      if (isANumber(token.attributes.type)) {
-        return 'base';
-      }
-      return token.attributes.type;
-    });
+          // Testing if is a base value, current used for sizes as the base types could be grouped together
+          const isANumber = string => /\d/.test(string);
+          if (isANumber(token.attributes.type)) {
+            return 'base';
+          }
+          return token.attributes.type;
+        });
 
     const tokensData = { color: groupedByColor, size: groupedBySize, font: groupedByFont };
     return JSON.stringify(tokensData, null, 2);
@@ -63,7 +60,7 @@ const getThemeConfig = themeName => {
       [platform]: {
         prefix: 'b2b',
         buildPath: `build/`,
-        transforms: ['attribute/cti', 'name/cti/kebab', 'color/css', 'size/rem'],
+        transforms: ['attribute/cti', 'name/kebab', 'color/css', 'size/rem'],
         files: [
           {
             destination: `css/${themeName}.css`,
@@ -92,7 +89,7 @@ console.log(`\nBuilding Design Tokens`);
 themes.map(function (theme) {
   console.log(`\n${theme} theme`);
 
-  const StyleDictionary = StyleDictionaryPackage.extend(getThemeConfig(theme));
+  const StyleDictionary = new StyleDictionaryPackage(getThemeConfig(theme));
   StyleDictionary.buildPlatform(platform);
 });
 console.log('\n==============================================');
