e058edcfe7268c510e47ca3e59530e717e6a9eec
Daniel Theuke <daniel.theuke@aequitas-software.de>
2025-09-23 15:11:05 +0200

    Revert "test(flyout-menu): disabled isn't clickable"
    This reverts commit 66533bee366905163ff32d056a5eab19303ca324.

--- packages/core-components/src/components/flyout-menu/flyout-menu.e2e.tsx
+++ packages/core-components/src/components/flyout-menu/flyout-menu.e2e.tsx
@@ -85,14 +85,10 @@ describe('B2B-FlyoutMenu', () => {
     const flyoutMenuOption = await page.find(
       'b2b-flyout-menu-option[option="option3"]',
     );
+    await flyoutMenuOption.click();
+    await page.waitForChanges();
 
     expect(flyoutMenuOption).not.toBeNull();
-    try {
-      await flyoutMenuOption.click();
-      await page.waitForChanges();
-    } catch {
-      // expected - not clickable
-    }
     expect(optionSelectedEventSpy).not.toHaveReceivedEvent();
   });
 
