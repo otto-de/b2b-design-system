10a356eab4d09b0829ac972e5227f951f50f76ad
Vivek Biswas <vivek.biswas@thoughtworks.com>
2025-12-02 20:06:47 +0530

    test(date-range-picker): [BLA-SCOUT] Fix date range picker value for consistent test out come

--- packages/core-components/src/components/date-range-picker/date-range-picker.spec.tsx
+++ packages/core-components/src/components/date-range-picker/date-range-picker.spec.tsx
@@ -3,6 +3,31 @@ import { h } from '@stencil/core';
 import { B2bDateRangePicker } from './date-range-picker';
 import { ParagraphComponent } from '../paragraph/paragraph';
 
+const MOCK_DATE = new Date('2025-11-15T12:00:00.000Z');
+const RealDate = Date;
+
+beforeAll(() => {
+  // @ts-ignore
+  global.Date = class extends RealDate {
+    constructor(...args) {
+      if (args.length === 0) {
+        super(MOCK_DATE.getTime());
+      } else {
+        // @ts-ignore
+        super(...args);
+      }
+    }
+
+    static now() {
+      return MOCK_DATE.getTime();
+    }
+  };
+});
+
+afterAll(() => {
+  global.Date = RealDate;
+});
+
 it('should render the date-range-picker', async () => {
   const page = await newSpecPage({
     components: [B2bDateRangePicker],
