3f66c8d40fc2fa9612c99759f73378f3a61994cb
Prashant Tripathi1 <tprashan@thoughtworks.com>
2026-07-03 16:42:17 +0530

    fix(testing): [BLA-270] | testing b2b access

--- packages/core-components/src/components/headline/headline.spec.tsx
+++ packages/core-components/src/components/headline/headline.spec.tsx
@@ -2,25 +2,6 @@ import { newSpecPage } from '@stencil/core/testing';
 import { h } from '@stencil/core';
 import { HeadlineComponent } from './headline';
 
-it('should use h1 tag with headline 400 size', async () => {
-  const page = await newSpecPage({
-    components: [HeadlineComponent],
-    template: () => (
-      <b2b-headline size="400">This is a h1 Headline</b2b-headline>
-    ),
-  });
-  expect(page.root).toEqualHtml(`
-    <b2b-headline>
-      <mock:shadow-root>
-        <h1 class="b2b-headline--400 b2b-headline--left">
-          <slot></slot>
-        </h1>
-      </mock:shadow-root>
-      This is a h1 Headline
-    </b2b-headline>
-  `);
-});
-
 it('should use h2 tag with headline 200 size', async () => {
   const page = await newSpecPage({
     components: [HeadlineComponent],
